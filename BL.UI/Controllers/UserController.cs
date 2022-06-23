4111629c790957c155154ebf9b4d343de7bd491d
NarasimhaRaoSomarouhtu <kapunarasimha143@gmail.com>
2022-06-23 16:38:33 +0530

    modified

---
+++ BL.UI/Controllers/UserController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BL.DataService;
+using BL.UI.Repositories;
+
+namespace BL.UI.Controllers
+{
+    [Route("controller")]
+    public class UserController : Controller
+    {
+        public UserRepository userRepository;
+
+        public UserController()
+        {
+            userRepository = new UserRepository();
+        }
+        [Route("GetAllUsers")]
+        public ActionResult Index()
+        {
+            List<User> users = userRepository.GetUsers();
+            return View(users);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(User user)
+        {
+            userRepository.addUser(user);
+            return RedirectToAction("Index");
+        }
+        public ActionResult Details(string userId)
+        {
+            User user = userRepository.GetUserById(userId);
+            return View(user);
+        }
+        public ActionResult Delete(string userId)
+        {
+            userRepository.DeleteUser(userId);
+            return RedirectToAction("Index");
+        }
+        public ActionResult Edit(string userId)
+        {
+            User user = userRepository.GetUserById(userId);
+            return View(user);
+        }
+        [HttpPost]
+        public ActionResult Edit(User user)
+        {
+            userRepository.EditUser(user);
+            return RedirectToAction("Index");
+        }
+    }
+}
