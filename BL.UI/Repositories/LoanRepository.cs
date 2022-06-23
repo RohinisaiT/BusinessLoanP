6740241f499d4696b4edb38eb7117d37cab69353
NarasimhaRaoSomarouhtu <kapunarasimha143@gmail.com>
2022-06-23 11:19:31 +0530

    added repository

--- BL.UI/Repositories/LoanRepository.cs
+++ BL.UI/Repositories/LoanRepository.cs
@@ -2,11 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using BL.DataService;
 
 namespace BL.UI.Repositories
 {
-    public class DocumentsRepository
+    public class Loan
     {
     }
 }
